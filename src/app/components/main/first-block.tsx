46742c124433b54fe6335d6a56054d61c913a624
Tony Paik <nakhyunpaik@gmail.com>
2024-06-05 13:50:08 -0700

    refactor

--- src/app/components/main/first-block.tsx
+++ src/app/components/main/first-block.tsx
@@ -1,26 +1,48 @@
 "use client";
 import BackgroundVideo from "../background-video";
+import Image from "next/image";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 
 //? 해야할일
-//~ 비디오 영상 밑에 이미지 파일 추가
-//~ 비디오 영상 끝나면 이미지 파일로 전환
-//~ 텍스트 에니메이션 효과
+//~ 글귀 애니메이션 추가
+//~ 비디오 끝나고 이미지로 변경될때 페이드인 효과 추가
 
 export default function FirstBlock() {
   const [currentSection, setCurrentSection] = useState(0);
+  const [showVideo, setShowVideo] = useState(true);
 
   useEffect(() => {
+    const videoDuration = 25000; // 10 seconds
+    const timer = setTimeout(() => {
+      setShowVideo(false);
+    }, videoDuration);
+
     const interval = setInterval(() => {
       setCurrentSection((prevSection) => (prevSection + 1) % 2);
     }, 3000);
-    return () => clearInterval(interval);
+
+    return () => {
+      clearTimeout(timer);
+      clearInterval(interval);
+    };
   }, []);
 
   return (
     <div className="relative min-h-screen">
-      <BackgroundVideo />
+      {showVideo ? (
+        <BackgroundVideo />
+      ) : (
+        <div className="absolute inset-0 z-0 w-full h-full">
+          <Image
+            src="/main-bg.png"
+            alt="Background Image"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
+      )}
       <div className="absolute inset-0 z-10">
         <div
           className={`flex flex-col items-center justify-center text-white text-center transition-transform transform-gpu duration-1000 ${
