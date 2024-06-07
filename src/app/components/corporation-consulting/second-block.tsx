de63d442c91f10d3898336fac97b358c32c3a569
Tony Paik <nakhyunpaik@gmail.com>
2024-06-07 15:08:36 -0700

    cor-consulting

--- src/app/components/corporation-consulting/second-block.tsx
+++ src/app/components/corporation-consulting/second-block.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function SecondBlock() {
   return (
-    <div className="relative bg-gray-800 py-16">
+    <div className="relative bg-gray-800 py-24">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
         <div className="text-white leading-relaxed">
           <h2 className="text-base text-blue-400 font-semibold tracking-wide uppercase">
