4c115565a9046b83df3a4e078b4ec59fd4d0a325
Tony Paik <nakhyunpaik@gmail.com>
2024-06-10 13:14:45 -0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <header className="sticky top-0 bg-white text-black shadow-md z-20">
           <NavBar />
